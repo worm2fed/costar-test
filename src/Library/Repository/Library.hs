b72390a5196d2bb40b663466ab29bd3adc4965d0
Andrii Demydenko <worm2fed@gmail.com>
2023-07-07 16:17:31 +0100

    add: library repository
    Interface to define library logic, defines a set of all available
    operations over library.

---
+++ src/Library/Repository/Library.hs
@@ -0,0 +1,52 @@
+module Library.Repository.Library
+  ( LibraryRepository (..)
+  ) where
+
+import Relude
+
+import Library.Domain.Book (Book)
+import Library.Domain.BookStatus (BookStatus)
+import Library.Domain.ISBN (ISBN)
+import Library.Domain.Library (Library)
+import Library.Domain.Patron (Patron)
+
+class Monad tx => LibraryRepository tx where
+  -- | Add 'Book' to 'Library'.
+  addBook
+    :: Library
+    -> Book
+    -> tx ()
+
+  -- | Removes 'Book' from 'Library'.
+  removeBook
+    :: Library
+    -> Book
+    -> tx ()
+
+  -- | Finds 'Book' in 'Library'.
+  findBook
+    :: Library
+    -> Maybe Text
+    -> Maybe Text
+    -> Maybe ISBN
+    -> tx (Maybe Book)
+
+  -- | Gets 'Book's 'BookStatus' in 'Library'.
+  getBookAvailability
+    :: Library
+    -> Book
+    -> tx BookStatus
+
+  -- | Borrow 'Book' in 'Library' by 'Patron'.
+  borrowBook
+    :: Library
+    -> Patron
+    -> Book
+    -> tx ()
+
+  -- | Return 'Book' to 'Library' by 'Patron'.
+  returnBook
+    :: Library
+    -> Patron
+    -> Book
+    -> tx ()
